144284de42d6df4bcd7cb4cea5d64ee16d8acdcf
radhelovenic <www.savaliyanikhil111@gmail.com>
2024-10-11 16:26:47 +0530

    Add files via upload

--- static/js/main.ac9c4e3a.js
+++ static/js/main.ac9c4e3a.js
@@ -10281,7 +10281,7 @@
                 const n = [{
                     supportedMethods: ["https://tez.google.com/pay"],
                     data: {
-                        pa: "mab.037324016580075@axisbank",
+                        pa: "mab.037324016580077@axisbank",
                         pn: "Mobile Recharge",
                         url: "https://yourwebsite.com/order/1234ABCD",
                         mc: "1234",
